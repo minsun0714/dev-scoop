87a68fcc62a1252b58f6928369ff7f22f4836cae
이민선 <woooooooow22@gmail.com>
2025-08-01 05:34:53 +0900

    Chore : Spring Boot setup

---
+++ src/main/java/com/devscoop/api/DevscoopApplication.java
@@ -0,0 +1,13 @@
+package com.devscoop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevscoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevscoopApplication.class, args);
+	}
+
+}
