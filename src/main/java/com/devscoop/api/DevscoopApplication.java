5e2f3a511942ee7af108bf34a744917b6ccfd593
이민선 <woooooooow22@gmail.com>
2025-08-02 13:58:52 +0900

    chore: Spring Boot 애플리케이션 @EnableScheduling 추가

--- src/main/java/com/devscoop/api/DevscoopApplication.java
+++ src/main/java/com/devscoop/api/DevscoopApplication.java
@@ -1,15 +1,21 @@
 package com.devscoop.api;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories
 public class DevscoopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevscoopApplication.class, args);
 	}
-
 }
