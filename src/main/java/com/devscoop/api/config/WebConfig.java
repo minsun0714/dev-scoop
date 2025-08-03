c7c5f15c29f82ad8527529acc18c13f0e50f104b
이민선 <woooooooow22@gmail.com>
2025-08-03 17:56:15 +0900

    fix : CORS 설정

---
+++ src/main/java/com/devscoop/api/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.devscoop.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 API 경로에 대해
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://43.202.45.36",  // EC2 퍼블릭 IP
+                                "https://dev-scoop.click" // 배포된 프론트 주소
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
