1728aa126499899c7cf8f807b11ea78eb38de311
이민선 <woooooooow22@gmail.com>
2025-08-03 18:34:02 +0900

    fix : 환경변수 주입

--- src/main/java/com/devscoop/api/config/RedisConfig.java
+++ src/main/java/com/devscoop/api/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.devscoop.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,8 +12,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port
+    ) {
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
