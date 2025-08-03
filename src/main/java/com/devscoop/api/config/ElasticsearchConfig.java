eb4100a6e83f6d0b254c6898b7ab04f46ad029ae
이민선 <woooooooow22@gmail.com>
2025-08-03 15:48:33 +0900

    config: Elasticsearch 설정을 외부 설정값 기반으로 변경
    - application.yml의 elasticsearch.host, elasticsearch.port 값을 @Value로 주입
    - RestClient 생성 시 localhost 고정 대신 설정 파일 값 사용
    - 환경에 따라 Elasticsearch 호스트와 포트를 쉽게 변경 가능하도록 개선

--- src/main/java/com/devscoop/api/config/ElasticsearchConfig.java
+++ src/main/java/com/devscoop/api/config/ElasticsearchConfig.java
@@ -6,22 +6,30 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Value("${elasticsearch.port}")
+    private int esPort;
+
     @Bean
     public ElasticsearchClient elasticsearchClient() {
         RestClient restClient = RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"))
-                .build();
+                new HttpHost(esHost, esPort, "http")
+        ).build();
 
         ElasticsearchTransport transport = new RestClientTransport(
-                restClient, new JacksonJsonpMapper());
+                restClient,
+                new JacksonJsonpMapper()
+        );
 
         return new ElasticsearchClient(transport);
     }
-
 }
