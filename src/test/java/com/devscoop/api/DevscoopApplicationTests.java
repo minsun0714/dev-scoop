cfdec47b3d800adba3ee8df0d9f69e5cce732411
이민선 <woooooooow22@gmail.com>
2025-08-06 21:42:00 +0900

    fix: 테스트에서 Elasticsearch Client 자체를 Mock 처리

--- src/test/java/com/devscoop/api/DevscoopApplicationTests.java
+++ src/test/java/com/devscoop/api/DevscoopApplicationTests.java
@@ -1,11 +1,14 @@
 package com.devscoop.api;
 
+import com.devscoop.api.config.MockEsClientConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(MockEsClientConfig.class)
 class DevscoopApplicationTests {
 
 	@Test
---
+++ src/test/java/com/devscoop/api/config/MockEsClientConfig.java
@@ -0,0 +1,14 @@
+package com.devscoop.api.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class MockEsClientConfig {
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        return Mockito.mock(ElasticsearchClient.class); // 또는 ES 인터페이스를 구현한 Stub
+    }
+}
